f082e600f1e931795faab36ae606016464529307
Janitha <janithalahiru23@gmail.com>
2025-04-18 07:53:18 +0530

    initializing the project✏

---
+++ controllers/productController.js
@@ -0,0 +1,70 @@
+import Product from "../models/products.js"
+import { isAdmin } from "./userController.js"
+
+
+export async function getProducts(req,res){
+   /* Product.find().then((data)=>{
+        res.json(data);
+    }).catch()*/
+    try{
+        if(isAdmin(req)){
+            const products=await Product.find()
+            res.json(products)
+        }else{
+           const products=await Product.find({isAvailable:true}) 
+           res.json(products)
+        }
+    }catch(err){
+        res.json({
+            message:"Failed to get products",
+            error:err
+        })
+    }
+} 
+
+export function saveProducts(req,res){
+
+    if(!isAdmin(req)){
+        res.status(403).json({
+            message:"You are not authorized to add a product"
+        })
+        return
+    }
+    const product=new Product(/*{
+        name:req.body.name,
+        price:req.body.price,
+        description:req.body.description 
+    }*/
+    req.body
+    )
+    product.save().then(()=>{
+        res.json({
+            message: "Product added successfully"
+        })
+    }).catch(()=>{
+        res.json({
+            message: "Failed to add product"
+        })
+    })
+}
+
+export async function deleteProducts(req,res){
+    if(!isAdmin(req)){
+        res.status(403).json({
+            message:"You are not authorized to add a product"
+        })
+        return
+    }
+    try{
+        await Product.deleteOne({productId:req.params.productId})
+
+        res.json({
+            message:"Product deleted successfully"
+        })
+    }catch(err){
+        res.status(500).json({
+            message:"Failed to delete product",
+            error:err
+        })
+    }
+}
